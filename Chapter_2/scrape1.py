a2ca1ba36c2e79689d5d2b713a65cff29c2231ac
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-11 21:17:40 +0000

    Add first scraping example from Chapter 2

---
+++ Chapter_2/scrape1.py
@@ -0,0 +1,8 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+bsObj = BeautifulSoup(html, "html.parser")
+
+namelist = bsObj.findAll("span", {"class", "green"})
+for name in namelist:
+	print(name.get_text()) # strip away the tags
