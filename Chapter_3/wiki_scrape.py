091e0dde47859844dab7b1a1046de1035797638c
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-15 16:23:10 +0000

    Wiki page link scrapper updates.

--- Chapter_3/wiki_scrape.py
+++ Chapter_3/wiki_scrape.py
@@ -1,12 +1,17 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import re
+import re, datetime, random
 
-html = urlopen("https://en.wikipedia.org/wiki/Kevin_Bacon")
-bsObj = BeautifulSoup(html, "html.parser")
+random.seed(datetime.datetime.now())
 
-# Get all links that poin to article pages
-for link in bsObj.find("div", {"id":"bodyContent"}).findAll("a",
-						href=re.compile("^(/wiki/)((?!:).)*$")):
-	if 'href' in link.attrs:
-		print(link.attrs["href"])
+def getLinks(articleUrl):
+	html = urlopen("https://en.wikipedia.org/" + articleUrl)
+	bsObj = BeautifulSoup(html)
+	return bsObj.find("div", id="bodyContent").findAll("a", 
+						{"href":re.compile("^(/wiki/)((?!:).)*$")})
+
+links = getLinks("/wiki/Kevin_Bacon")
+while len(links) > 0:
+	newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+	print(newArticle)
+	links = getLinks(newArticle)
