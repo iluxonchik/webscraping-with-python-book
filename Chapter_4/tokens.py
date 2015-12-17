6656c31194405a880a419a0b242047aa19e33667
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-17 18:30:26 +0000

    Add file with oaut h parameter definintions.

---
+++ Chapter_4/tokens.py
@@ -0,0 +1,5 @@
+# Contains the tokens and secrets used in OAuth
+ACCESS_TOKEN = "your access token"
+ACCESS_TOKEN_SECRET = "your access token secret"
+CONSUMER_KEY = "your consumer key"
+CONSUMER_SECRET = "your consumer secret"
