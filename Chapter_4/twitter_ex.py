33c7c32b9621354ee63a9fba0c7e9a161560d30e
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-17 18:52:31 +0000

    Add search for tweets example

--- Chapter_4/twitter_ex.py
+++ Chapter_4/twitter_ex.py
@@ -2,5 +2,6 @@ import tokens
 from twitter import *
 
 t = Twitter(auth=OAuth(tokens.ACCESS_TOKEN, tokens.ACCESS_TOKEN_SECRET, tokens.CONSUMER_KEY, tokens.CONSUMER_SECRET))
-status_update = t.statuses.update(status='API test')
-print(status_update)
+# status_update = t.statuses.update(status='API test')
+python_tweets = t.search.tweets(q = "#python")
+print(python_tweets)
