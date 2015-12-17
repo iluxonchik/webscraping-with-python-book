a6cf092bcef17efc70879abc1035eacf27fa084a
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-17 18:57:37 +0000

    Add get user tweets example

--- Chapter_4/twitter_ex.py
+++ Chapter_4/twitter_ex.py
@@ -3,5 +3,7 @@ from twitter import *
 
 t = Twitter(auth=OAuth(tokens.ACCESS_TOKEN, tokens.ACCESS_TOKEN_SECRET, tokens.CONSUMER_KEY, tokens.CONSUMER_SECRET))
 # status_update = t.statuses.update(status='API test')
-python_tweets = t.search.tweets(q = "#python")
-print(python_tweets)
+# python_tweets = t.search.tweets(q = "#python")
+statuses = t.statuses.user_timeline(screen_name="ILUXONCHIK", count=5)
+
+print(statuses)
