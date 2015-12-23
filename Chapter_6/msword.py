cee0beb37a0d3adccb7a857942f3a91a9b5442a6
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-23 14:00:05 +0000

    Add docx parsing example

---
+++ Chapter_6/msword.py
@@ -0,0 +1,24 @@
+from zipfile import ZipFile
+from urllib.request import urlopen
+from io import BytesIO
+from bs4 import BeautifulSoup
+
+wordFile = urlopen("http://pythonscraping.com/pages/AWordDocument.docx").read()
+wordFile = BytesIO(wordFile)
+document = ZipFile(wordFile)
+xml_content = document.read("word/document.xml")
+
+wordObj = BeautifulSoup(xml_content.decode("utf-8"), "html.parser")
+textStrings = wordObj.findAll("w:t")
+for textElem in textStrings:
+    closeTag = ""
+    try:
+        style = textElem.parent.previousSibling.find("w:pstyle")
+        if style is not None and style["w:val"] == "Title":
+            print("<h1>")
+            closeTag = "</h1>"
+    except AttributeError:
+        pass
+
+    print(textElem.text)
+    print(closeTag)
