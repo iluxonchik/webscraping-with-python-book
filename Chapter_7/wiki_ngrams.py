cdace40cc756bc07a7cea135ad5b33203c326207
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-01-06 21:10:53 +0000

    Add n-grams counter

--- Chapter_7/wiki_ngrams.py
+++ Chapter_7/wiki_ngrams.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from collections import OrderedDict
 import re, string
 
 def clean_input(input):
@@ -8,6 +9,7 @@ def clean_input(input):
     input = re.sub('\[[0-9]*\]', '', input) # remove numbers enclosed by brackets
     input = bytes(input, 'UTF-8')
     input = input.decode('ascii', 'ignore')
+    input = input.lower()
     clean_input = []
 
     input = input.split(' ')
@@ -25,9 +27,21 @@ def ngrams(input, n):
         output.append(input[i:i+n])
     return output
 
+def ngrams_cnt(input, n):
+    input = clean_input(input)
+    output = {}
+    for i in range(len(input)-n+1):
+        key = repr(input[i:i+n])
+        if key in output:
+            output[key] += 1
+        else:
+            output[key] = 1
+    return output
+
 html = urlopen('http://en.wikipedia.org/wiki/Python_(programming_language)')
 bsObj = BeautifulSoup(html, 'html.parser')
 content = bsObj.find('div', {'id':'mw-content-text'}).get_text()
-ngrams = ngrams(content, 2)
+ngrams = ngrams_cnt(content, 2)
+ngrams = OrderedDict(sorted(ngrams.items(), key=lambda t: t[1], reverse=True))
 print(ngrams)
 print("2-gram count is " + str(len(ngrams)))
