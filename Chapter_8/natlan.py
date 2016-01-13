3bbc810468f4d9c6ad949ab4a75897497530e0cd
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-01-13 22:25:02 +0000

    Chapter 8 completed

--- Chapter_8/natlan.py
+++ Chapter_8/natlan.py
@@ -1,10 +1,11 @@
-from nltk import word_tokenize
-from nltk import Text
-from nltk import FreqDist, bigrams, ngrams
+from nltk import FreqDist, bigrams, ngrams, pos_tag, word_tokenize, sent_tokenize
 from nltk.book import text6
 
-tokens = word_tokenize("Here is some not very interesting text")
-text = Text(tokens)
+tokens = word_tokenize("Here is some not very interesting text. Let's get its tags!")
+print(pos_tag(tokens))
+
+tokens = word_tokenize("The dust was thick so he had to dust.")
+print(pos_tag(tokens))
 
 fdist = FreqDist(text6)
 print(fdist.most_common(10))
@@ -20,3 +21,13 @@ print(fourgramsDist[("father", "smelt", "of", "elderberries")])
 for fourgram in fourgrams:
     if fourgram[0] == "coconut":
         print(fourgram)
+
+sentences = sent_tokenize("Google is one of the best companies in the world. People constantly google stuff.")
+nouns = ["NN", "NNS", "NNP", "NNPS"]
+
+for sentence in sentences:
+    if "google" in sentence.lower():
+        taggedWords = pos_tag(word_tokenize(sentence))
+        for word in taggedWords:
+            if word[0].lower() == "google" and word[1] in nouns:
+                print(sentence)
