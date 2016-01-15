f7418ab33d3e015660d48011350178bd3f108de2
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-01-15 09:18:02 +0000

    Add selenium demos from Chapter 10

---
+++ Chapter_10/seleniumdemo2.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.PhantomJS(executable_path="C:/phantomjs/bin/phantomjs.exe")
+driver.get("http://pythonscraping.com/pages/javascript/ajaxDemo.html")
+try:
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "loadedButton")))
+finally:
+    print(driver.find_element(By.ID, "content").text)
+    driver.close()
